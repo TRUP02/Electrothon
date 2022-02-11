fcb1fca02714c3b3d3f89616a9b5935ca24251dd
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-02-11 12:25:30 +0530

    created model

---
+++ models/machine.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const machineSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('machineSchema')
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const machine = require('../models/machine')
+
+
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+router.post('/:id', (req, res) => {
+
+})
+
+router.patch('/', (req, res) => {
+
+})
 
 module.exports = router
