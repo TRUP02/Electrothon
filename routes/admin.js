1d539a38148608ae313e25c3f4b614369b0ede3c
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-02-11 12:53:15 +0530

    added get request

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const router = express.Router()
-const machine = require('../models/machine')
+const Machine = require('../models/machine')
 
+router.get('/', async (req, res) => {
+    try {
+        const machines = await Machine.find()
+        res.status(200).json(machines)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 
-router.post('/:id', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const machine = new Machine({
+        id: req.body.id,
+        name: req.body.name,
+        type: req.body.type
+    })
+    try {
+        const newMachine = await machine.save()
+        res.status(201).json(newMachine)
+    } catch (e) {
+        res.status(400).json({ message: e.message })
+    }
 })
 
 router.patch('/', (req, res) => {
