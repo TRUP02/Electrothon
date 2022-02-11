5d55e963dfcbaf5028b78fb5da23720f72a269a5
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-02-11 12:05:21 +0530

    Setup mongodb atlas

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', (e) => console.error(e))
+db.on('open', () => console.log("Connected to db"))
+
+app.listen(4000, () => {
+    console.log("Server started at port 4000")
+})
